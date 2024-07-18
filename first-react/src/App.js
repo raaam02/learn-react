28b974b7ae4576f9817e5300cfec3a21b15ae781
raaam02 <kingk9869991@gmail.com>
2024-07-18 14:28:11 +0530

    first react app init

---
+++ first-react/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [value, setValue] = useState(0);
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
