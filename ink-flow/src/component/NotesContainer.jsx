c0385a56df569f10c83c7533e4f92a0d008e1d8d
ramLogicalWings <ram.logicalwings@gmail.com>
2024-08-24 21:37:56 +0530

    NoteCard created

--- ink-flow/src/component/NotesContainer.jsx
+++ ink-flow/src/component/NotesContainer.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Addnote from "./Addnote";
+import NoteCard from "./NoteCard";
 
 function NotesContainer() {
   return (
     <div className="">
       <Addnote />
+      <div className="m-14 w-100">
+        <NoteCard />
+        <NoteCard />
+      </div>
     </div>
   );
 }
